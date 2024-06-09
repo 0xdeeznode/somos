d8269665c9e3c93c8786cfb3c0c4e7d2dedaaf12
0xdeeznode <nalanarud@gmail.com>
2024-06-08 19:44:56 -0500

    views work

--- somoseco/urls.py
+++ somoseco/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'somoseco'  # Define the namespace
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register', views.register_view, name='register'),
+    path('logout', views.logout_view, name='logout'),
+    path('login', views.login_view, name='login'),
 ]
--- somoseco/views.py
+++ somoseco/views.py
@@ -1,5 +1,52 @@
 from django.shortcuts import render
+from django.contrib.auth import logout, login, authenticate
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from .models import User, Post, Campaign
+
+
 def index(request):
     return render(request, 'somoseco/index.html')
+
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Password match check
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "somoseco/register.html", {
+                "message": "Passwords don't match."
+            })
+        
+        user = User.objects.create_user(username, password, email)
+        user.save()
+        login(request, user)
+        return HttpResponseRedirect(reverse("somoseco:index"))
+    else:
+        return render(request, "somoseco/register.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("somoseco:index"))
+        else:
+            return render(request, "somoseco/login.html", {
+                "message": "Invalid credentials."
+            })
+    else:
+        return render(request, "somoseco/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("somoseco:index"))
